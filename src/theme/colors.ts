3a72003b01a4a70e24a0ffd06c050384dff33995
Aman Ullah <amanix007@gmail.com>
2022-06-05 16:17:11 +0600

    first commit

---
+++ src/theme/colors.ts
@@ -0,0 +1,22 @@
+const variables = {
+  black: '#000',
+  white: '#fff',
+  primary: {
+    main: '#713BDB',
+  },
+  secondary: {
+    light: '#61678F',
+    main: '#8547AD',
+    // dark: "#2e7031",
+    contrastText: '#000',
+  },
+  error: '#F51D53',
+  success: '#00A357',
+  warning: '#FFDA47',
+  background: '#E1E3E6',
+  textColor1: '#1B1B3D',
+  textColor2: '#61678F',
+  lineColor: '#e0e2f1',
+}
+
+export default variables
---
+++ src/theme/theme.ts
@@ -0,0 +1,96 @@
+import {
+  createTheme,
+  responsiveFontSizes,
+  ThemeOptions,
+} from '@mui/material/styles'
+import colors from './colors'
+
+declare module '@mui/material/styles' {
+  interface Theme {
+    status: {
+      danger: string
+    }
+  }
+  // allow configuration using `createTheme`
+  interface ThemeOptions {
+    status?: {
+      danger?: string
+    }
+  }
+}
+
+const options: ThemeOptions = {
+  palette: {
+    primary: colors.primary,
+    secondary: colors.secondary,
+  },
+
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        '@global': {
+          html: {
+            fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
+            fontWeight: 400,
+            lineHeight: 1.5,
+            letterSpacing: '0.00938em',
+            WebkitFontSmoothing: 'auto',
+          },
+        },
+      },
+    },
+    MuiPaginationItem: {
+      styleOverrides: {
+        root: {
+          '&.Mui-selected': {
+            color: colors.white,
+          },
+        },
+      },
+    },
+    MuiTouchRipple: {
+      styleOverrides: {
+        child: {
+          backgroundColor: colors.white,
+        },
+      },
+    },
+    MuiTypography: {
+      styleOverrides: {
+        root: {
+          // fontFamily: `"Bangla", "Roboto", "sans-serif" !important`,
+        },
+      },
+    },
+    MuiPagination: {
+      styleOverrides: {
+        root: {
+          color: colors.white,
+        },
+      },
+    },
+    MuiContainer: {
+      styleOverrides: {
+        maxWidthLg: {
+          '@media (min-width: 1024px)': {
+            maxWidth: 'calc(1120px + 48px)',
+          },
+        },
+      },
+    },
+  },
+
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 900,
+      lg: 1200,
+      xl: 1536,
+    },
+  },
+}
+
+const theme = responsiveFontSizes(createTheme(options))
+
+export default theme
