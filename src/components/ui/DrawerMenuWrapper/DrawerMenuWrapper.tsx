d8df2963971cdfbd404de2cb78b494bc47aed0a0
Aman Ullah <amanix007@gmail.com>
2022-06-05 16:50:30 +0600

    working on basic UI

---
+++ src/components/ui/DrawerMenuWrapper/DrawerMenuWrapper.tsx
@@ -0,0 +1,164 @@
+import * as React from 'react'
+import Typography from '@mui/material/Typography'
+import CssBaseline from '@mui/material/CssBaseline'
+import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar'
+import Toolbar from '@mui/material/Toolbar'
+
+import MenuIcon from '@mui/icons-material/Menu'
+
+import { styled, useTheme } from '@mui/material/styles'
+import Drawer from '@mui/material/Drawer'
+import List from '@mui/material/List'
+
+import Divider from '@mui/material/Divider'
+import IconButton from '@mui/material/IconButton'
+
+import ChevronRightIcon from '@mui/icons-material/ChevronRight'
+import ListItem from '@mui/material/ListItem'
+import ListItemButton from '@mui/material/ListItemButton'
+import ListItemIcon from '@mui/material/ListItemIcon'
+import ListItemText from '@mui/material/ListItemText'
+
+import SupervisedUserCircleIcon from '@mui/icons-material/SupervisedUserCircle'
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft'
+
+import { useRouter } from 'next/router'
+
+const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })<{
+  open?: boolean
+}>(({ theme, open }) => ({
+  flexGrow: 1,
+  padding: theme.spacing(3),
+  transition: theme.transitions.create('margin', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  marginLeft: `-${drawerWidth}px`,
+  ...(open && {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
+  }),
+}))
+
+interface AppBarProps extends MuiAppBarProps {
+  open?: boolean
+}
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})<AppBarProps>(({ theme, open }) => ({
+  transition: theme.transitions.create(['margin', 'width'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: `${drawerWidth}px`,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}))
+
+const drawerWidth = 240
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: 'flex-end',
+}))
+
+type Props = {
+  children: React.ReactNode
+}
+const menuList = [
+  {
+    text: 'User Account',
+    icon: <SupervisedUserCircleIcon />,
+    href: '/user-account',
+  },
+]
+export default function DrawerMenu({ children }: Props) {
+  const [open, setOpen] = React.useState(true)
+
+  const handleDrawerOpen = () => {
+    setOpen(true)
+  }
+
+  const handleDrawerClose = () => {
+    setOpen(false)
+  }
+  const router = useRouter()
+  const theme = useTheme()
+  return (
+    <>
+      <CssBaseline />
+      <AppBar position="fixed" open={open}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={{ mr: 2, ...(open && { display: 'none' }) }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap component="div">
+            nextjs-ts-mui-linting-boilerplate
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Main open={open}>
+        <DrawerHeader />
+        {children}
+      </Main>
+
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box',
+          },
+        }}
+        variant="persistent"
+        anchor="left"
+        open={open}
+      >
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? (
+              <ChevronLeftIcon />
+            ) : (
+              <ChevronRightIcon />
+            )}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List>
+          {menuList.map((menuItem) => (
+            <ListItem
+              onClick={() => router.push(menuItem.href)}
+              key={menuItem.text}
+              disablePadding
+            >
+              <ListItemButton>
+                <ListItemIcon>{menuItem.icon}</ListItemIcon>
+                <ListItemText primary={menuItem.text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+    </>
+  )
+}
--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -25,70 +25,70 @@ const options: ThemeOptions = {
     secondary: colors.secondary,
   },
 
-  components: {
-    MuiCssBaseline: {
-      styleOverrides: {
-        '@global': {
-          html: {
-            fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
-            fontWeight: 400,
-            lineHeight: 1.5,
-            letterSpacing: '0.00938em',
-            WebkitFontSmoothing: 'auto',
-          },
-        },
-      },
-    },
-    MuiPaginationItem: {
-      styleOverrides: {
-        root: {
-          '&.Mui-selected': {
-            color: colors.white,
-          },
-        },
-      },
-    },
-    MuiTouchRipple: {
-      styleOverrides: {
-        child: {
-          backgroundColor: colors.white,
-        },
-      },
-    },
-    MuiTypography: {
-      styleOverrides: {
-        root: {
-          // fontFamily: `"Bangla", "Roboto", "sans-serif" !important`,
-        },
-      },
-    },
-    MuiPagination: {
-      styleOverrides: {
-        root: {
-          color: colors.white,
-        },
-      },
-    },
-    MuiContainer: {
-      styleOverrides: {
-        maxWidthLg: {
-          '@media (min-width: 1024px)': {
-            maxWidth: 'calc(1120px + 48px)',
-          },
-        },
-      },
-    },
-  },
+  // components: {
+  //   MuiCssBaseline: {
+  //     styleOverrides: {
+  //       '@global': {
+  //         html: {
+  //           fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
+  //           fontWeight: 400,
+  //           lineHeight: 1.5,
+  //           letterSpacing: '0.00938em',
+  //           WebkitFontSmoothing: 'auto',
+  //         },
+  //       },
+  //     },
+  //   },
+  //   MuiPaginationItem: {
+  //     styleOverrides: {
+  //       root: {
+  //         '&.Mui-selected': {
+  //           color: colors.white,
+  //         },
+  //       },
+  //     },
+  //   },
+  //   MuiTouchRipple: {
+  //     styleOverrides: {
+  //       child: {
+  //         backgroundColor: colors.white,
+  //       },
+  //     },
+  //   },
+  //   MuiTypography: {
+  //     styleOverrides: {
+  //       root: {
+  //         // fontFamily: `"Bangla", "Roboto", "sans-serif" !important`,
+  //       },
+  //     },
+  //   },
+  //   MuiPagination: {
+  //     styleOverrides: {
+  //       root: {
+  //         color: colors.white,
+  //       },
+  //     },
+  //   },
+  //   MuiContainer: {
+  //     styleOverrides: {
+  //       maxWidthLg: {
+  //         '@media (min-width: 1024px)': {
+  //           maxWidth: 'calc(1120px + 48px)',
+  //         },
+  //       },
+  //     },
+  //   },
+  // },
 
-  breakpoints: {
-    values: {
-      xs: 0,
-      sm: 600,
-      md: 900,
-      lg: 1200,
-      xl: 1536,
-    },
-  },
+  // breakpoints: {
+  //   values: {
+  //     xs: 0,
+  //     sm: 600,
+  //     md: 900,
+  //     lg: 1200,
+  //     xl: 1536,
+  //   },
+  // },
 }
 
 const theme = responsiveFontSizes(createTheme(options))
